e38e2e0fcc74b10c8def2a5408f4becc963eecc6
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-11-20 21:36:51 +0000

    initial commit

---
+++ app/footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+type Props = {}
+
+const Footer = (props: Props) => {
+  return (
+    <div>
+      {/* Footer */}
+      <footer>
+       
+      </footer>
+    </div>
+  )
+}
+export default Footer
