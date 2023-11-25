d9eb4bb2e4f24adf682194bc13ef1d65cd7a3439
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-11-25 23:56:27 +0000

    release page

--- release/page.tsx
+++ release/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
       <div className="text-center text-white">
         <p className="text-sm font-semibold uppercase tracking-wide">luma*</p>
         <h1 className="text-4xl font-bold mt-2">
-          Luma iOS App, Luma Calendars, and Event Updates
+          Luma iOS App,xLuma Calendars, and Event Updates
         </h1>
         <p className="text-base font-medium mt-4">Fall 2023 Release</p>
       </div>
