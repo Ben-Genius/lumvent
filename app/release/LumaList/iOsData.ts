6be4447e7aa83bd245e05cf1b6be1d6e4f396cd0
Ben-Genius <bernard.wiafe@afrilogicsolutions.com>
2023-12-23 23:23:38 +0000

    license objeect list

---
+++ app/release/LumaList/iOsData.ts
@@ -0,0 +1,51 @@
+const iosAppList1 = [
+    {
+        'title': 'Create events',
+        'para': 'Create events right from your phone, and easily access events you are hosting or going to.'
+        
+    },
+    {
+        'title': 'Send SMS invites',
+        'para': 'Invite people in your phone book and past guests effortlessly via text message or email.        '
+        
+    },
+    {
+        'title': 'Notifications',
+        'para': 'Keep up with your chats, event invites, and event updates.'
+        
+    },
+]
+
+const iosAppList2 = [
+    {
+        'title': 'Chat with (new) friends',
+        'para': 'Attend events, make friends, and chat with them right from the app.        ',
+        'icon':'',
+    },
+    {
+        'title': 'Check in guests',
+        'para': 'Check in guests using a QR code or from a list. Apple Wallet compatible.        ',
+        'icon':'',
+    },
+
+    {
+        'title': 'Explore your city',
+        'para': 'Discover featured calendars and popular events in your city.        ',
+        'icon':'',
+    },
+    {
+        'title': 'Event feed',
+        'para': "Easily see what events you’re going to and what's happening around you.",
+        'icon':'',
+    },
+    {
+        'title': 'Guest list',
+        'para': "See who’s coming to an event before you register",
+        'icon':'',
+    },
+    {
+        'title': 'Weather forecast',
+        'para': 'Easily check the weather and pack an umbrella if needed',
+        'icon':'',
+    },
+]
